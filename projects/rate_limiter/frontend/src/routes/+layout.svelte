34e541cc6086a6ade7180a79ccd9af154c50e93d
Jelani Jackson <jelani.jackson@zapier.com>
2025-04-07 04:38:24 -0400

    Rate limiting start

--- projects/rate_limiter/frontend/src/routes/+layout.svelte
+++ projects/rate_limiter/frontend/src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid-rows-20 grid grid-cols-1" style="width: 100vw; height: 100vh;">
+	<div class="col-span-full text-center">Header thing here</div>
+
+	<div class="row-span-19 grid grid-cols-8">
+		<div>
+			<nav class="grid text-center text-white gap-2">
+				<a class="nav-link bg-blue-500 rounded-md" href="/">Home</a>
+				<a class="nav-link bg-blue-500 rounded-md" href="/sliding-window">Sliding Window</a>
+			</nav>
+		</div>
+
+		<main class="col-span-5 col-start-3">
+			{@render children()}
+		</main>
+	</div>
+</div>
